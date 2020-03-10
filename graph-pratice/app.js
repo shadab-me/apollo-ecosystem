37480c921c68f83062e3ce6e5292e129fd19fb0a
Shadab Ali <shadabali604@gmail.com>
2020-03-10 17:29:39 +0530

    book query added

--- graph-pratice/app.js
+++ graph-pratice/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
-
+const graphqlHTTP = require('express-graphql');
 const app = express();
 
 
+app.use('', graphqlHTTP({
+
+
+
+}));
 app.listen(3000, () => {
     console.log('Connecting')
 })
